73bcd9eac6167c3c35ed705911768d3df1909fa7
Stephan Hillebrand <junkmail@hit-to-key.net>
2020-05-04 02:54:46 +0100

    note to voct computation

---
+++ src/voct.cpp
@@ -0,0 +1,13 @@
+#include "voct.h"
+
+#define VALUE_PER_NOTE 47.0690
+unsigned short midi_to_cv(int midi){
+	if (midi < 21) {
+		midi = 21;
+	} else if (midi > 108) {
+		midi = 108;
+	}
+	float note = midi - 21;
+	return (unsigned int) ((float) note * VALUE_PER_NOTE);
+}
+
---
+++ src/voct.h
@@ -0,0 +1,6 @@
+#ifndef __INCLUDE_VOCT_H_
+#define __INCLUDE_VOCT_H_
+
+unsigned short midi_to_cv(int midi);
+
+#endif
